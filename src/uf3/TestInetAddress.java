300c2f2530b4b49ce5aa25759d0a57cbabd71ecf
Alumne <Alumne@104-PC05>
2019-11-22 18:09:27 +0100

    1.2

--- src/uf3/TestInetAddress.java
+++ src/uf3/TestInetAddress.java
@@ -22,9 +22,10 @@ public class TestInetAddress {
 			//Array tipus InetAddress amb totes les adreces IP de google.com
 			System.out.println("\tAdreces IP per a: "+dir.getHostName());
 			InetAddress[] adreces = InetAddress.getAllByName(dir.getHostName());
+			System.out.println("\tMètode getAllByName():");
 			for (int i=0; i<adreces.length; i++) 
 				System.out.println("\t\t"+adreces[i].toString());
-			System.out.println("Retorna un array amb totes les adreces IP que té el host");
+			System.out.println("\tRetorna un array amb totes les adreces IP que té el host");
 			System.out.println("=====================================================");
 			
 		} catch (UnknownHostException e1) {e1.printStackTrace();}
@@ -35,26 +36,26 @@ public class TestInetAddress {
 		
 		InetAddress dir2;
 		
-		System.out.println("\tMètode getByName(): "+dir);
+		System.out.println("\tMètode getByName(): "+dir+" --> Retorna el nom del host i la seva IP");
 		
 		try {
 			dir2 = InetAddress.getLocalHost();
-			System.out.println("\tMètode getLocalHost(): "+dir2);
-			System.out.println("Retorna el nom i la IP del PC des d'on executem aquest programa");
+			System.out.print("\tMètode getLocalHost(): "+dir2);
+			System.out.println(" --> Retorna el nom i la IP del PC des d'on executem aquest programa");
 		} catch (UnknownHostException e) {e.printStackTrace();}
 		
 		//FEM SERVIR MÊTODES DE LA CLASSE
 		System.out.println("\tMètode getHostName(): "+dir.getHostName()+" -->Retorna el nom del host");
 		System.out.println("\tMètode getHostAddress(): "+dir.getHostAddress()+" -->Retorna la IP del host");
-		System.out.println("\tMètode toString(): "+dir.toString()+" ");
-		System.out.println("\tMètode getCanonicalHostName(): "+dir.getCanonicalHostName());
+		System.out.println("\tMètode toString(): "+dir.toString()+" --> Converteix a String el nom del host i la seva IP");
+		System.out.println("\tMètode getCanonicalHostName(): "+dir.getCanonicalHostName()+ " --> Retorna el nom complet del domini");
 		byte[] getAddress = dir.getAddress();
 		System.out.print("\tMètode getAddress(): ");
 		for (int i = 0; i < getAddress.length-1; i++) {
 			System.out.print(getAddress[i]+".");
 		}
 		System.out.print(getAddress[3]);
-		System.out.println(" Retorna un array en bits de l'adreça IP");
+		System.out.println(" --> Retorna un array en bits de l'adreça IP");
 		
 	}
 }
